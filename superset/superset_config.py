4c52ab622f4628aef8e91cd1fd4414710e38c09a
Aleqyan666 <haykalekyan@gmail.com>
2024-08-13 10:13:38 +0400

    Adding al components of the apache superset microservice

---
+++ superset/superset_config.py
@@ -0,0 +1,26 @@
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+}
+
+ENABLE_PROXY_FIX = True
+SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_STRING"
+
+APP_NAME = "VivaSuperset"
+# APP_ICON = "/favicon.png"
+# LOGO_TARGET_PATH = '/superset-logo-horiz.png'
+
+
+# Specify tooltip that should appear when hovering over the App Icon/Logo
+# LOGO_TOOLTIP = "VivaSuperset"
+# #Customization
+# #Set the logo
+# LOGO_TARGET_PATH = 'superset/viva_pic.png'
+# # Set the primary color for the Superset UI
+# PRIMARY_COLOR = '#e30613'
+# #Set the app name and welcome message
+# APP_NAME = 'VivaSuperset'
+# WELCOME_MESSAGE = """
+# # Welcome to My Custom Superset Application
+
+# We are excited to have you here! Explore your data and gain insights with our powerful tools.
+# """
